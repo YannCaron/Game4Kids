e6da2e4b3f3ac573915d5916f5d1ef53a33eb899
cyann <cyann74@gmail.com>
2018-04-23 22:11:53 +0200

    For var issue #44

--- core/blockly/generators/javascript.js
+++ core/blockly/generators/javascript.js
@@ -4,8 +4,17 @@ Blockly.JavaScript.initialize = function(workspace){
 }
 
 Blockly.JavaScript.finalize = function(workspace) {
+    var variableNames = workspace.getAllVariables()
+        .filter(variable => variable.type != Blockly.Block.ACTOR_TYPE)
+        .map (variable => variable.name);
+
     Blockly.JavaScript.definitions_['variables'] =
         'var game4k = new Game4kids.Game(' + Blockly.Block.GAME_WIDTH + ', ' + Blockly.Block.GAME_HEIGHT + ', \'content_game\', { preload: preload, create: create});';
+        
+    if (variableNames.length > 0) {
+        Blockly.JavaScript.definitions_['variables'] +=
+            '\nvar ' + variableNames.join(', ') + ';';
+    }
 
 }
 
