527b35cce30b2a12e5926f17569e3e9807087a87
cyann <cyann74@gmail.com>
2018-04-29 06:43:40 +0200

    no signal if no actor

--- core/blockly/event_dynamic.js
+++ core/blockly/event_dynamic.js
@@ -22,7 +22,7 @@ Blockly.eventDynamic.SIGNAL_IF =
     '</block>';
 
 Blockly.eventDynamic.SIGNAL_EVERY =
-    '<block type="signal_every">' + 
+    '<block type="signal_every">' +
     Blockly.dynamic.buildShadowNumber('VALUE', 0.75) +
     '</block>';
 
@@ -38,9 +38,9 @@ Blockly.eventDynamic.SIGNAL_CREATE_COLLIDE =
 Blockly.eventDynamic.SIGNAL_COLLIDE =
     '<block type="signal_collide"></block>';
 
-    /*
+/*
 Blockly.eventDynamic.SIGNAL_WITH =
-    '<block type="signal_with"></block>';
+'<block type="signal_with"></block>';
 */
 Blockly.eventDynamic.eventFlyoutCallback = function (workspace) {
 
@@ -48,33 +48,32 @@ Blockly.eventDynamic.eventFlyoutCallback = function (workspace) {
 
     var actorVariables = workspace.getVariablesOfType(Blockly.Block.ACTOR_TYPE);
 
-    // constructor
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_CONSTRUCTOR)));
-
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_CREATE));
-
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_DESTROY));
-
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_PROPERTIES)));
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_PROPERTY));
-
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_OPERATOR)));
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_COMBINE));
-
-//Blockly.eventDynamic.SIGNAL_COUNT
-
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_METHODS)));
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_IF));
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_EVERY));
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_MOUSE));
-    xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_KEYBOARD));
-
     if (actorVariables.length > 0) {
+        // constructor
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_CONSTRUCTOR)));
+
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_CREATE));
+
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_DESTROY));
+
+        // property
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_PROPERTIES)));
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_PROPERTY));
+
+        // operator
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_OPERATOR)));
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_COMBINE));
+
+        // methods
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_METHODS)));
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_IF));
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_EVERY));
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_MOUSE));
+        xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_KEYBOARD));
+
         xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_COLLIDE));
-        //xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_WITH));
-    }
 
-    if (actorVariables.length > 0) {
+        // static
         xmlList.push(Blockly.Xml.xmlToDom(Blockly.dynamic.buildLabel(Blockly.Msg.OBJECT_STATIC)));
         xmlList.push(Blockly.Xml.xmlToDom(Blockly.eventDynamic.SIGNAL_CREATE_COLLIDE));
     }
