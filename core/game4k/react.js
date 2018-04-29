f23cf8f90b05bb733510d5a64abff3c22d28194f
cyann <cyann74@gmail.com>
2018-04-29 13:00:38 +0200

    emit parameters (don't works)

--- core/game4k/react.js
+++ core/game4k/react.js
@@ -1,7 +1,7 @@
 // namespace
 Game4kids.React = Game4kids.React || {
 
-    valueOf: function(f) {
+    valueOf: function (f) {
         if (typeof f === 'function') return f();
         else return f;
     },
@@ -41,7 +41,8 @@ Game4kids.React.Signal.prototype.emit = function (value) {
     this.time = Game4kids.current.game.time.now;
     this.duration = (Game4kids.current.game.time.now - this.now) / 1000
     if (this.callback) {
-        this.callback(value);
+        arguments[0] = value;
+        this.callback(...arguments);
     }
 }
 
@@ -51,7 +52,8 @@ Game4kids.React.Signal.prototype.filter = function (predicate) {
 
     this.subscribe(function (value) {
         if (predicate.call(this, value)) {
-            signal.emit(value);
+            arguments[0] = value;
+            signal.emit(...arguments);
         }
     });
 
@@ -63,7 +65,7 @@ Game4kids.React.Signal.prototype.toggle = function () {
 
     return this.filter(function (value) {
         if (typeof self.previous === 'undefined') self.previous = null;
-        
+
         if (value != self.previous) {
             self.previous = value;
             return true;
@@ -73,7 +75,7 @@ Game4kids.React.Signal.prototype.toggle = function () {
 
 }
 
-Game4kids.React.Signal.prototype.every = function(interval) {
+Game4kids.React.Signal.prototype.every = function (interval) {
     var self = this;
 
     return this.filter(function (value) {
@@ -117,11 +119,13 @@ Game4kids.React.Signal.prototype.combineWith = function (signal2) {
     var signal = new Game4kids.React.Signal(this);
 
     this.subscribe(function (value) {
-        signal.emit(value);
+        arguments[0] = value;
+        signal.emit(...arguments);
     });
 
     signal2.subscribe(function (value) {
-        signal.emit(value);
+        arguments[0] = value;
+        signal.emit(...arguments);
     });
 
     return signal;
@@ -132,14 +136,15 @@ Game4kids.React.Signal.prototype.map = function (mapper) {
     var signal = new Game4kids.React.Signal(this);
 
     this.subscribe(function (value) {
-        signal.emit(mapper.bind(this)() || false);
+        arguments[0] = mapper.bind(this)() || false;
+        signal.emit(...arguments);
     });
 
     return signal;
 }
 
 Game4kids.React.Signal.prototype.toObject = function (object) {
-    return this.map(function() { return object; });
+    return this.map(function () { return object; });
 }
 
 Game4kids.React.Signal.prototype.toTime = function () {
@@ -165,12 +170,15 @@ Game4kids.Game.prototype.updateEvent = function () {
     this.count++;
 
     // loop on signals
-    this.signals.forEach (signal => signal.emit(this.count));
+    this.signals.forEach(signal => {
+        arguments[0] = this.count;
+        signal.emit(...arguments);
+    });
 
 }
 
 // method
-Game4kids.Game.prototype.createSignal = function(actor = null) {
+Game4kids.Game.prototype.createSignal = function (actor = null) {
     var signal = new Game4kids.React.Signal(null, actor);
     this.signals.push(signal);
 
@@ -205,7 +213,7 @@ Game4kids.Game.prototype.removeActorSignals = function (actor) {
     var signals = this.actorSignals.get(actor);
     this.actorSignals.delete(actor);
 
-    signals.forEach (signal => {
+    signals.forEach(signal => {
         this.removeSignal(signal);
     });
 }
@@ -213,30 +221,3 @@ Game4kids.Game.prototype.removeActorSignals = function (actor) {
 Game4kids.Game.prototype.clearSignals = function () {
     this.signals = [];
 }
-
-/*
-Game4kids.React.Signal.prototype.toEvent = function (object) {
-    var signal = new Game4kids.React.Signal(this);
-    var value = false;
-
-    this.subscribe(function (value) {
-        signal.emit(value);
-    });
-
-    factory(function (value) {
-        value = true;
-    });
-
-    return signal;
-}
-
-Game4kids.Game.prototype.createSignalFromEvent = function (factory) {
-    var signal = new Game4kids.React.Signal();
-
-    factory(function (value) {
-        signal.emit(value);
-    });
-
-    return signal;
-}
-*/
